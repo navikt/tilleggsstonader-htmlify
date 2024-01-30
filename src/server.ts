13327b4e832d95d113e155ba952830177963cc39
Johan Blomgren <blommish@users.noreply.github.com>
2024-01-30 16:00:38 +0100

    Setter riktig port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import path from 'path';
 
 import logger from './felles/logger';
 import routes from './routes';
@@ -8,19 +7,14 @@ import routes from './routes';
 dotenv.config();
 export const { NODE_ENV } = process.env;
 
-const buildDir = path.join(process.cwd() + '/public');
 const app = express();
-
-if (NODE_ENV === 'production') {
-    app.use(express.static(buildDir));
-}
 
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', routes);
 
-const port = 8001;
+const port = NODE_ENV === 'development' ? 8001 : 3000;
 app.listen(port, () => {
     logger.info(`Server now listening on port: ${port}`);
 });
