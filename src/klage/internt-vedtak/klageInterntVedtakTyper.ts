597c5bf9e58a5f4cb5fa2ea9268b4f8781987a2e
Johan Blomgren <blommish@users.noreply.github.com>
2025-02-27 10:30:32 +0100

    Mapper hjemler til tekst på lik måte som i sak-frontend

--- src/klage/internt-vedtak/klageInterntVedtakTyper.ts
+++ src/klage/internt-vedtak/klageInterntVedtakTyper.ts
@@ -128,50 +128,6 @@ export const årsakTilTekst: Record<EÅrsakOmgjøring, string> = {
     ANNET: 'Annet',
 };
 
-enum FolketrygdlovenHjemmelEF {
-    FT_FEMTEN_TO = 'FT_FEMTEN_TO',
-    FT_FEMTEN_TRE = 'FT_FEMTEN_TRE',
-    FT_FEMTEN_FIRE = 'FT_FEMTEN_FIRE',
-    FT_FEMTEN_FEM = 'FT_FEMTEN_FEM',
-    FT_FEMTEN_SEKS = 'FT_FEMTEN_SEKS',
-    FT_FEMTEN_ÅTTE = 'FT_FEMTEN_ÅTTE',
-    FT_FEMTEN_NI = 'FT_FEMTEN_NI',
-    FT_FEMTEN_TI = 'FT_FEMTEN_TI',
-    FT_FEMTEN_ELLEVE = 'FT_FEMTEN_ELLEVE',
-    FT_FEMTEN_TOLV = 'FT_FEMTEN_TOLV',
-    FT_FEMTEN_TRETTEN = 'FT_FEMTEN_TRETTEN',
-    FT_TJUETO_TOLV = 'FT_TJUETO_TOLV',
-    FT_TJUETO_TRETTEN = 'FT_TJUETO_TRETTEN',
-    FT_TJUETO_FEMTEN = 'FT_TJUETO_FEMTEN',
-}
-
-enum BarnetrygdlovenHjemmel {
-    BT_TO = 'BT_TO',
-    BT_FIRE = 'BT_FIRE',
-    BT_FEM = 'BT_FEM',
-    BT_NI = 'BT_NI',
-    BT_TI = 'BT_TI',
-    BT_ELLEVE = 'BT_ELLEVE',
-    BT_TOLV = 'BT_TOLV',
-    BT_TRETTEN = 'BT_TRETTEN',
-    BT_SYTTEN = 'BT_SYTTEN',
-    BT_ATTEN = 'BT_ATTEN',
-}
-
-enum KontantstøttelovenHjemmel {
-    KS_TO = 'KS_TO',
-    KS_TRE = 'KS_TRE',
-    KS_SEKS = 'KS_SEKS',
-    KS_SYV = 'KS_SYV',
-    KS_ÅTTE = 'KS_ÅTTE',
-    KS_NI = 'KS_NI',
-    KS_TI = 'KS_TI',
-    KS_ELLEVE = 'KS_ELLEVE',
-    KS_TOLV = 'KS_TOLV',
-    KS_TRETTEN = 'KS_TRETTEN',
-    KS_SEKSTEN = 'KS_SEKSTEN',
-}
-
 enum UtlandsavtalerHjemmel {
     UTLAND_EØS = 'UTLAND_EØS',
     UTLAND_EØS_FORORDNINGEN_FEM = 'UTLAND_EØS_FORORDNINGEN_FEM',
@@ -191,7 +147,8 @@ enum FolketrygdlovenHjemmelTS {
     FTRL_11_A_4 = 'FTRL_11_A_4',
     FTRL_11_A_4_3 = 'FTRL_11_A_4_3',
     FTRL_15_11 = 'FTRL_15_11',
-    FTRL_17_10_17_15 = 'FTRL_17_10_17_15',
+    FTRL_17_10 = 'FTRL_17_10',
+    FTRL_17_15 = 'FTRL_17_15',
     FTRL_21_12 = 'FTRL_21_12',
     FTRL_22_13 = 'FTRL_22_13',
     FTRL_22_15 = 'FTRL_22_15',
@@ -224,61 +181,12 @@ enum ForvaltningslovenHjemmel {
 }
 
 export type Hjemmel =
-    | FolketrygdlovenHjemmelEF
-    | BarnetrygdlovenHjemmel
-    | KontantstøttelovenHjemmel
     | UtlandsavtalerHjemmel
     | ArbeidsmarkedslovenHjemmel
     | FolketrygdlovenHjemmelTS
     | TilleggsstønadforskriftenHjemmel
     | ForeldeseslovenHjemmel
     | ForvaltningslovenHjemmel;
-
-const folketrygdLovenEFVisningstekster: Record<FolketrygdlovenHjemmelEF, string> = {
-    FT_FEMTEN_TO: '§ 15-2 Forutgående medlemskap',
-    FT_FEMTEN_TRE: '§ 15-3 Opphold i Norge',
-    FT_FEMTEN_FIRE: '§ 15-4 Enslig mor eller far',
-    FT_FEMTEN_FEM: '§ 15-5 Overgangsstønad',
-    FT_FEMTEN_SEKS: '§ 15-6 Plikt til yrkesrettet aktivitet',
-    FT_FEMTEN_ÅTTE: '§ 15-8 Stønadsperiode',
-    FT_FEMTEN_NI: '§ 15-9 Avkorting mot inntekt',
-    FT_FEMTEN_TI: '§ 15-10 Stønad til barnetilsyn',
-    FT_FEMTEN_ELLEVE: '§ 15-11 Stønad til skolepenger',
-    FT_FEMTEN_TOLV: '§ 15-12 Sanksjon',
-    FT_FEMTEN_TRETTEN: '§ 15-13 Forholdet til andre folketrygdytelser',
-    FT_TJUETO_TOLV:
-        '§ 22-12 Tidspunkt for utbetaling når rett til en ytelse oppstår eller opphører',
-    FT_TJUETO_TRETTEN:
-        '§ 22-13 Frister for framsetting av krav, virkningstidspunkt og etterbetaling',
-    FT_TJUETO_FEMTEN: '§ 22-15 Tilbakekreving',
-};
-
-const barnetrygdlovenVisningstekster: Record<BarnetrygdlovenHjemmel, string> = {
-    BT_TO: '§ 2 Hvem som har rett til barnetrygd',
-    BT_FIRE: '§ 4 Bosatt i riket, lovlig opphold mm.',
-    BT_FEM: '§ 5 Medlemskap i folketrygden under utenlandsopphold',
-    BT_NI: '§ 9 Utvidet barnetrygd',
-    BT_TI: '§ 10 Barnetrygdens størrelse',
-    BT_ELLEVE: '§ 11 Stønadsperiode',
-    BT_TOLV: '§ 12 Utbetaling',
-    BT_TRETTEN: '§ 13 Tilbakekreving',
-    BT_SYTTEN: '§ 17 Stønadsmottakers opplysningsplikt',
-    BT_ATTEN: '§ 18 Uriktige opplysninger',
-};
-
-const kontantstøttelovenVisningstekster: Record<KontantstøttelovenHjemmel, string> = {
-    KS_TO: '§ 2 Vilkår knyttet til barnet',
-    KS_TRE: '§ 3 Vilkår knyttet til støttemottaker',
-    KS_SEKS: '§ 6 Barn i fosterhjem eller institusjon',
-    KS_SYV: '§ 7 Kontantstøttens størrelse',
-    KS_ÅTTE: '§ 8 Stønadsperiode',
-    KS_NI: '§ 9 Utbetaling av kontantstøtte - delt bosted',
-    KS_TI: '§ 10 Utbetaling til adopterte barn',
-    KS_ELLEVE: '§ 11 Tilbakekreving',
-    KS_TOLV: '§ 12 Støttemottakerens opplysningsplikt',
-    KS_TRETTEN: '§ 13 Avslag på søknad, stans i utbetalingen',
-    KS_SEKSTEN: '§ 16 Opplysningsplikt',
-};
 
 const utlandsavtalerVisningstekster: Record<UtlandsavtalerHjemmel, string> = {
     UTLAND_EØS: 'EØS-avtalen',
@@ -289,52 +197,50 @@ const utlandsavtalerVisningstekster: Record<UtlandsavtalerHjemmel, string> = {
 };
 
 const arbeidsmarkedslovenVisningstekster: Record<ArbeidsmarkedslovenHjemmel, string> = {
-    ARBML_13: 'ARBML_13',
-    ARBML_17: 'ARBML_17',
-    ARBML_22: 'ARBML_22',
+    ARBML_13: 'Arb.mark.lov § 13',
+    ARBML_17: 'Arb.mark.lov § 17',
+    ARBML_22: 'Arb.mark.lov § 22',
 };
 
-const folketrygdlovenTSVisningstekster: Record<FolketrygdlovenHjemmelTS, string> = {
-    FTRL_11_A_3: 'FTRL_11_A_3',
-    FTRL_11_A_4: 'FTRL_11_A_4',
-    FTRL_11_A_4_3: 'FTRL_11_A_4_3',
-    FTRL_15_11: 'FTRL_15_11',
-    FTRL_17_10_17_15: 'FTRL_17_10_17_15',
-    FTRL_21_12: 'FTRL_21_12',
-    FTRL_22_13: 'FTRL_22_13',
-    FTRL_22_15: 'FTRL_22_15',
-    FTRL_22_17A: 'FTRL_22_17A',
+export const folketrygdlovenTSVisningstekster: Record<FolketrygdlovenHjemmelTS, string> = {
+    FTRL_11_A_3: 'Ftrl. § 11 A-3',
+    FTRL_11_A_4: 'Ftrl. § 11 A-4',
+    FTRL_11_A_4_3: 'Ftrl. § 11 A-4 3. ledd',
+    FTRL_15_11: 'Ftrl. § 15-11',
+    FTRL_17_10: 'Ftrl. § 17-10',
+    FTRL_17_15: 'Ftrl. § 17-15',
+    FTRL_21_12: 'Ftrl. § 21-12',
+    FTRL_22_13: 'Ftrl. § 22-13',
+    FTRL_22_15: 'Ftrl. § 22-15',
+    FTRL_22_17A: 'Ftrl. § 22-17a',
 };
 
 const tilleggsstønadforskriftenVisningstekster: Record<TilleggsstønadforskriftenHjemmel, string> = {
-    FS_TILL_ST_1_3_MOBILITET: 'FS_TILL_ST_1_3_MOBILITET',
-    FS_TILL_ST_3_REISE: 'FS_TILL_ST_3_REISE',
-    FS_TILL_ST_6_FLYTTING: 'FS_TILL_ST_6_FLYTTING',
-    FS_TILL_ST_8_BOLIG: 'FS_TILL_ST_8_BOLIG',
-    FS_TILL_ST_10_TILSYN: 'FS_TILL_ST_10_TILSYN',
-    FS_TILL_ST_12_LAEREMIDLER: 'FS_TILL_ST_12_LAEREMIDLER',
-    FS_TILL_ST_15_2: 'FS_TILL_ST_15_2',
-    FS_TILL_ST_15_3: 'FS_TILL_ST_15_3',
+    FS_TILL_ST_1_3_MOBILITET: 'Til.st.forskr. § 1 3. ledd',
+    FS_TILL_ST_3_REISE: 'Til.st.forskr § 3',
+    FS_TILL_ST_6_FLYTTING: 'Til.st.forskr. § 6',
+    FS_TILL_ST_8_BOLIG: 'Til.st.forskr. § 8',
+    FS_TILL_ST_10_TILSYN: 'Til.st.forskr. § 10',
+    FS_TILL_ST_12_LAEREMIDLER: 'Til.st.forskr. § 12',
+    FS_TILL_ST_15_2: 'Til.st.forskr § 15 2. ledd',
+    FS_TILL_ST_15_3: 'Til.st.forskr. § 15 3. ledd',
 };
 
 const foreldeseslovenVisningstekster: Record<ForeldeseslovenHjemmel, string> = {
-    FL_2_3: 'FL_2_3',
-    FL_10: 'FL_10',
+    FL_2_3: 'Foreld.lov §§ 2 og 3',
+    FL_10: 'Foreld.lov § 10',
 };
 
 const forvaltningslovenVisningstekster: Record<ForvaltningslovenHjemmel, string> = {
-    FVL_11: 'FVL_11',
-    FVL_17: 'FVL_17',
-    FVL_18_19: 'FVL_18_19',
-    FVL_35: 'FVL_35',
-    FVL_41: 'FVL_41',
-    FVL_42: 'FVL_42',
+    FVL_11: 'Fvl. § 11',
+    FVL_17: 'Fvl. § 17 ',
+    FVL_18_19: 'Fvl. §§ 18 og 19 ',
+    FVL_35: 'Fvl. § 35',
+    FVL_41: 'Fvl. § 41 ',
+    FVL_42: 'Fvl. § 42',
 };
 
 export const hjemmelTilVisningstekst: Record<Hjemmel, string> = {
-    ...folketrygdLovenEFVisningstekster,
-    ...barnetrygdlovenVisningstekster,
-    ...kontantstøttelovenVisningstekster,
     ...utlandsavtalerVisningstekster,
     ...arbeidsmarkedslovenVisningstekster,
     ...folketrygdlovenTSVisningstekster,
