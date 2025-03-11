f66c468749c21ed695199c1797d20d760a769dbb
Sigmund Berbom <sberbom@gmail.com>
2025-03-11 13:44:29 +0100

    👔 Ikke vis stønadsperioder når bruker vedtaksperioder

--- src/internt-vedtak/StønadsperioderContent.tsx
+++ src/internt-vedtak/StønadsperioderContent.tsx
@@ -17,28 +17,30 @@ const StønadsperiodeRad: React.FC<{ periode: Stønadsperiode }> = ({ periode })
 };
 
 const StønadsperioderContent: React.FC<{
-    perioder: Stønadsperiode[];
+    perioder?: Stønadsperiode[];
 }> = ({ perioder }) => {
-    return (
-        <NonBreakingDiv className={'stonadsperioder'}>
-            <h2>Overlappsperioder</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Målgruppe</th>
-                        <th>Aktivitet</th>
-                        <th>Fra</th>
-                        <th>Til</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {perioder.map((periode, index) => (
-                        <StønadsperiodeRad key={index} periode={periode} />
-                    ))}
-                </tbody>
-            </table>
-        </NonBreakingDiv>
-    );
+    if (perioder) {
+        return (
+            <NonBreakingDiv className={'stonadsperioder'}>
+                <h2>Overlappsperioder</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Målgruppe</th>
+                            <th>Aktivitet</th>
+                            <th>Fra</th>
+                            <th>Til</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {perioder.map((periode, index) => (
+                            <StønadsperiodeRad key={index} periode={periode} />
+                        ))}
+                    </tbody>
+                </table>
+            </NonBreakingDiv>
+        );
+    }
 };
 
 export default StønadsperioderContent;
--- src/internt-vedtak/typer/interntVedtak.ts
+++ src/internt-vedtak/typer/interntVedtak.ts
@@ -11,7 +11,7 @@ export interface InterntVedtak {
     søknad?: Søknad;
     målgrupper: Vilkårperiode[];
     aktiviteter: Vilkårperiode[];
-    stønadsperioder: Stønadsperiode[];
+    stønadsperioder?: Stønadsperiode[];
     vedtaksperioder: Vedtaksperiode[];
     vilkår: Vilkår[];
     beregningsresultat?: Beregningsresultat;
