603f9d1976eb29d961533e76df7de4d433f3b362
Runar Skagestad <runar.skagestad@nav.no>
2025-04-01 16:52:07 +0200

    ✨ Map beregningstabell for boutgifter

--- src/internt-vedtak/typer/beregningsresultat.ts
+++ src/internt-vedtak/typer/beregningsresultat.ts
@@ -1,6 +1,7 @@
 export interface Beregningsresultat {
     tilsynBarn?: BeregningsresultatTilsynBarn[];
     læremidler?: BeregningsresultatLæremidler[];
+    boutgifter?: BeregningsresultatBoutgifter[];
 }
 
 export interface BeregningsresultatTilsynBarn {
@@ -14,6 +15,15 @@ export interface BeregningsresultatTilsynBarn {
 }
 
 export interface BeregningsresultatLæremidler {
+    fom: string;
+    tom: string;
+    antallMåneder: number;
+    beløp: number;
+    stønadsbeløp: number;
+    utbetalingsdato: string;
+}
+
+export interface BeregningsresultatBoutgifter {
     fom: string;
     tom: string;
     antallMåneder: number;
