09fe5a8e8b7e6972d59e0f17cd0ec0b08d82ea68
Nibedita.Pahari <nibedita.pahari@nav.no>
2025-10-27 00:32:47 +0100

    oppdatert vilkårtype

--- src/internt-vedtak/typer/vilkår.ts
+++ src/internt-vedtak/typer/vilkår.ts
@@ -25,7 +25,7 @@ export enum Vilkårtype {
     UTGIFTER_OVERNATTING = 'UTGIFTER_OVERNATTING',
     LØPENDE_UTGIFTER_EN_BOLIG = 'LØPENDE_UTGIFTER_EN_BOLIG',
     LØPENDE_UTGIFTER_TO_BOLIGER = 'LØPENDE_UTGIFTER_TO_BOLIGER',
-    DAGLIG_REISE_OFFENTLIG_TRANSPORT = 'DAGLIG_REISE_OFFENTLIG_TRANSPORT',
+    DAGLIG_REISE = 'DAGLIG_REISE',
 }
 
 export const vilkårtypeTilTekst: Record<Vilkårtype, string> = {
@@ -33,7 +33,7 @@ export const vilkårtypeTilTekst: Record<Vilkårtype, string> = {
     LØPENDE_UTGIFTER_EN_BOLIG: 'Løpende utgifter én bolig',
     LØPENDE_UTGIFTER_TO_BOLIGER: 'Løpende utgifter to boliger',
     UTGIFTER_OVERNATTING: 'Utgifter til overnatting',
-    DAGLIG_REISE_OFFENTLIG_TRANSPORT: 'Daglig reise offentlig transport',
+    DAGLIG_REISE: 'Daglig reise',
 };
 
 export enum Vilkårsresultat {
