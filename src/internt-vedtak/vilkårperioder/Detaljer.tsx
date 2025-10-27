641951493d06d33755e8f785b1aca7105ba7776d
Nibedita.Pahari <nibedita.pahari@nav.no>
2025-10-27 15:46:47 +0100

    oppdatert “Har bruker utgifter til ${stønadType}”  vilkår på aktivitet

--- src/internt-vedtak/vilkårperioder/Detaljer.tsx
+++ src/internt-vedtak/vilkårperioder/Detaljer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { formaterPeriode } from '../../felles/datoFormat';
+import { Stønadstype } from '../../felles/stønadstype';
 import { tekstEllerFeil } from '../../felles/tekstutils';
 import { FaktaOgVurdering } from '../FaktaOgVurdering';
 import { Begrunnelse } from '../felles';
@@ -9,14 +10,18 @@ import { kildeVilkårperiodeTilTekst, Vilkårperiode } from '../typer/vilkårper
 
 export const Detaljer: React.FC<{
     periode: Vilkårperiode;
-}> = ({ periode }) => {
+    stønadType: Stønadstype;
+}> = ({ periode, stønadType }) => {
     return (
         <div className={'vilkaarperiode-rad-content'}>
             <div>Periode: {formaterPeriode(periode)}</div>
             <div>Kilde: {tekstEllerFeil(kildeVilkårperiodeTilTekst, periode.kilde)}</div>
             <Begrunnelse begrunnelse={periode.begrunnelse} />
             <KommentarSlettet data={periode} />
-            <FaktaOgVurdering faktaOgVurderinger={periode.faktaOgVurderinger} />
+            <FaktaOgVurdering
+                faktaOgVurderinger={periode.faktaOgVurderinger}
+                stønadType={stønadType}
+            />
         </div>
     );
 };
--- src/internt-vedtak/vilkårperioder/VilkårperioderContent.tsx
+++ src/internt-vedtak/vilkårperioder/VilkårperioderContent.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { NonBreakingDiv } from '../felles';
 import { Detaljer } from './Detaljer';
 import { TypeOgResultat } from './TypeOgResultat';
+import { Stønadstype } from '../../felles/stønadstype';
 import { Vilkårperiode } from '../typer/vilkårperiode';
 
 export const VilkårperioderContent: React.FC<{
     navn: 'Målgrupper' | 'Aktiviteter';
     perioder: Vilkårperiode[];
-}> = ({ navn, perioder }) => {
+    stønadType: Stønadstype;
+}> = ({ navn, perioder, stønadType }) => {
     return (
         <NonBreakingDiv className={'vilkaarperiode'}>
             <h2>{navn}</h2>
@@ -18,7 +20,7 @@ export const VilkårperioderContent: React.FC<{
                     key={indeks}
                 >
                     <TypeOgResultat periode={periode} />
-                    <Detaljer periode={periode} />
+                    <Detaljer periode={periode} stønadType={stønadType} />
                 </NonBreakingDiv>
             ))}
         </NonBreakingDiv>
