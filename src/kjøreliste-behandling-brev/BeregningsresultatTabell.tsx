1593fc1efe8a03016c5454948341f49f04346723
sarahjelle <sara.hjelle@bekk.no>
2026-04-30 11:44:14 +0200

    Styling. Rydd opp i utseende på tabell

--- src/kjøreliste-behandling-brev/BeregningsresultatTabell.tsx
+++ src/kjøreliste-behandling-brev/BeregningsresultatTabell.tsx
@@ -5,32 +5,59 @@ import { kronerMedTusenSkilleEllerStrek } from '../felles/tekstutils';
 export const BeregningsresultatTabell: React.FC<{
     oppsummertReise: OppsummertBeregningForReise;
 }> = ({ oppsummertReise }) => {
+    const harBompengeutgifter = oppsummertReise.perioder.some(
+        (periode) => periode.bompengerTotalt && periode.bompengerTotalt > 0
+    );
+
+    const harFergekostnader = oppsummertReise.perioder.some(
+        (periode) => periode.fergekostnadTotalt && periode.fergekostnadTotalt > 0
+    );
+
     return (
-        <table style={{ fontSize: '90%', width: '100%' }}>
-            <thead>
-                <tr>
-                    <th>Uke</th>
-                    <th>Periode</th>
-                    <th>Antall dager som dekkes</th>
-                    <th>Totale bompengekostnader</th>
-                    <th>Totale fergekostnader</th>
-                    <th>Totale parkeringsutgifter</th>
-                    <th>Stønadsbeløp</th>
-                </tr>
-            </thead>
-            <tbody>
-                {oppsummertReise.perioder.map((periode, index) => (
-                    <tr key={`${periode.fom}-${periode.tom}-${index}`}>
-                        <td>Uke {periode.ukenummer}</td>
-                        <td>{formaterPeriode(periode)}</td>
-                        <td>{periode.antallGodkjenteReisedager}</td>
-                        <td>{kronerMedTusenSkilleEllerStrek(periode.bompengerTotalt)}</td>
-                        <td>{kronerMedTusenSkilleEllerStrek(periode.fergekostnadTotalt)}</td>
-                        <td>{kronerMedTusenSkilleEllerStrek(periode.parkeringskostnadTotalt)}</td>
-                        <td>{kronerMedTusenSkilleEllerStrek(periode.stønadsbeløp)}</td>
+        <div>
+            <table style={{ fontSize: '89%' }}>
+                <thead>
+                    <tr>
+                        <th>Uke</th>
+                        <th>Periode</th>
+                        <th>Antall dager</th>
+                        {harBompengeutgifter && <th>Bompenger</th>}
+                        {harFergekostnader && <th>Fergekostnad</th>}
+                        <th>Parkeringskostnad</th>
+                        <th>Stønadsbeløp</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {oppsummertReise.perioder.map((periode, index) => (
+                        <tr key={`${periode.fom}-${periode.tom}-${index}`}>
+                            <td>{periode.ukenummer}</td>
+                            <td style={{ minWidth: '140px' }}>{formaterPeriode(periode)}</td>
+                            <td>{periode.antallGodkjenteReisedager}</td>
+                            {harBompengeutgifter && (
+                                <td className="høyrejustert">
+                                    {kronerMedTusenSkilleEllerStrek(periode.bompengerTotalt)}
+                                </td>
+                            )}
+                            {harFergekostnader && (
+                                <td className="høyrejustert">
+                                    {kronerMedTusenSkilleEllerStrek(periode.fergekostnadTotalt)}
+                                </td>
+                            )}
+                            <td className="høyrejustert">
+                                {kronerMedTusenSkilleEllerStrek(periode.parkeringskostnadTotalt)}
+                            </td>
+                            <td className="høyrejustert">
+                                {kronerMedTusenSkilleEllerStrek(periode.stønadsbeløp)}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <p className="tabell-beskrivelse">
+                Antall dager viser hvor mange dager innenfor perioden hvor kjøring med privat bil
+                dekkes. Parkeringskostnad, fergekostnad og bompenger er oppgitt som totalsum for
+                hele uken.
+            </p>
+        </div>
     );
 };
