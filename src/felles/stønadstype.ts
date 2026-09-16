b75078c75eb564bb96988bfe9ef3787d1e16ab2e
Nibedita.Pahari <nibedita.pahari@nav.no>
2026-09-16 22:34:55 +0200

    internt vedtak reise til samling.

--- src/felles/stønadstype.ts
+++ src/felles/stønadstype.ts
@@ -4,6 +4,8 @@ export enum Stønadstype {
     BOUTGIFTER = 'BOUTGIFTER',
     DAGLIG_REISE_TSO = 'DAGLIG_REISE_TSO',
     DAGLIG_REISE_TSR = 'DAGLIG_REISE_TSR',
+    REISE_TIL_SAMLING_TSO = 'REISE_TIL_SAMLING_TSO',
+    REISE_TIL_SAMLING_TSR = 'REISE_TIL_SAMLING_TSR',
 }
 
 export const stønadstypeTilTekst: Record<Stønadstype, string> = {
@@ -12,6 +14,8 @@ export const stønadstypeTilTekst: Record<Stønadstype, string> = {
     BOUTGIFTER: 'boutgifter',
     DAGLIG_REISE_TSO: 'daglige reiser',
     DAGLIG_REISE_TSR: 'daglige reiser',
+    REISE_TIL_SAMLING_TSO: 'reiser til samlingsaktiviteter',
+    REISE_TIL_SAMLING_TSR: 'reiser til samlingsaktiviteter',
 };
 
 export const tittelInterntVedtak = (stønadstype: Stønadstype): string => {
