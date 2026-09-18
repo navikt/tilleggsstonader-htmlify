ec5d319347e48e80ff5d4de2748bf8f2a072914b
Nibedita.Pahari <nibedita.pahari@nav.no>
2026-09-18 13:38:08 +0200

    forkjellige komponentene for offenli transport og privat bil

---
+++ src/internt-vedtak/beregningsresultat/PrivatBilSamlingTabell.tsx
@@ -0,0 +1,48 @@
+import { formaterNorskDato } from '../../felles/datoFormat';
+import { BeregningsresultatPrivatBil } from '../typer/beregningsresultat';
+
+export const PrivatBilSamlingTabell: React.FC<{
+    samlinger: BeregningsresultatPrivatBil[];
+}> = ({ samlinger }) => {
+    return (
+        <>
+            {samlinger.map((samling, reiseIndex) => (
+                <div key={reiseIndex} style={{ marginBottom: '2rem' }}>
+                    <h3>{samling.adresse}</h3>
+
+                    <table style={{ fontSize: '75%' }}>
+                        <thead>
+                            <tr>
+                                <th>Fom</th>
+                                <th>Tom</th>
+                                <th>Totalt reiseavstand</th>
+                                <th>Sats</th>
+                                {samling.bompenger != null && <th>Bompenger</th>}
+                                {samling.parkering != null && <th>Parkering</th>}
+                                {samling.fergekostnad != null && <th>Fergekostnad</th>}
+                                {samling.piggdekkavgift != null && <th>Piggdekkavgift</th>}
+                                <th>Beløp.</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>{formaterNorskDato(samling.fom)}</td>
+                                <td>{formaterNorskDato(samling.tom)}</td>
+                                <td>{samling.totaltReiseavstand}</td>
+                                <td>{samling.sats}</td>
+                                {samling.bompenger != null && <td>{samling.bompenger}</td>}
+                                {samling.parkering != null && <td>{samling.parkering}</td>}
+                                {samling.fergekostnad != null && <td>{samling.fergekostnad}</td>}
+                                {samling.piggdekkavgift != null && (
+                                    <td>{samling.piggdekkavgift}</td>
+                                )}
+                                <td>{samling.beløp}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            ))}
+        </>
+    );
+};
